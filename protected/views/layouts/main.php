3631dc0c66b5088a020640983ff0833363ef2fb8
Dmitrij <dmitrijbelikov@gmail.com>
2013-12-07 00:49:32 +0400

    CSS, Gitignore

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -14,8 +14,6 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.css" />
-    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.print.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
--- protected/views/meeting/index.php
+++ protected/views/meeting/index.php
@@ -4,9 +4,7 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<div id="calendar">
-
-</div>
+<div id="calendar"></div>
 
 <div id="event" style="display: none;">
     <div id="start_date"></div><br />
@@ -19,8 +17,11 @@ $this->pageTitle=Yii::app()->name;
     <?= CHtml::label('Тип', 'type'); ?>
     <?= CHtml::dropDownList('type', '', MeetingType::getAll(), array('id' => 'type')); ?> <br />
 </div>
+
 <?php Yii::app()->clientScript->registerScriptFile('//code.jquery.com/jquery-1.9.1.js', CClientScript::POS_END); ?>
 <?php Yii::app()->clientScript->registerScriptFile('//code.jquery.com/ui/1.10.3/jquery-ui.js', CClientScript::POS_END); ?>
 <?php Yii::app()->clientScript->registerCssFile('//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css'); ?>
 <?php Yii::app()->clientScript->registerScriptFile('//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.min.js', CClientScript::POS_END); ?>
+<?php Yii::app()->clientScript->registerCssFile('//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.css'); ?>
+<?php Yii::app()->clientScript->registerCssFile('//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.print.css'); ?>
 <?php Yii::app()->clientScript->registerScriptFile('/js/pages/index.js', CClientScript::POS_END);
